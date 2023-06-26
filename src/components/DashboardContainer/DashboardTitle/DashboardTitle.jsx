6863fbc33049002dc09b9e1008eafac2d75d41ed
emanuelpps <emanuelpages.ps@gmail.com>
2023-06-26 16:40:43 -0300

    modificaciones en title y favicon

--- src/components/DashboardContainer/DashboardTitle/DashboardTitle.jsx
+++ src/components/DashboardContainer/DashboardTitle/DashboardTitle.jsx
@@ -4,7 +4,7 @@ import '../DashboardContainer.css';
 export default function DashboardTitle() {
   return (
     <div className='dashboard-title'>
-        <h3>EP Weather</h3>
+        <a href='https://emanuelpps.github.io/'><h3>EP Weather</h3></a>
     </div>
   )
 }
