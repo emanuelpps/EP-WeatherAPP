872120aee5869ceba7d9a38ec6061e85db818835
Emanuel Pagés <101524982+emanuelpps@users.noreply.github.com>
2024-02-20 21:29:16 -0300

    Update DashboardTitle.jsx

--- src/components/DashboardContainer/DashboardTitle/DashboardTitle.jsx
+++ src/components/DashboardContainer/DashboardTitle/DashboardTitle.jsx
@@ -4,7 +4,7 @@ import '../DashboardContainer.css';
 export default function DashboardTitle() {
   return (
     <div className='dashboard-title'>
-        <a href='https://emanuelpps.github.io/'><h3>EP Weather</h3></a>
+        <a href='https://emanuelp-portfolio.vercel.app/'><h3>EP Weather</h3></a>
     </div>
   )
 }
